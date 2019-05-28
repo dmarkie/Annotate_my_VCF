cd34d660b7d76b1f952276acf182a9fe2641a987
dmarkie <david.markie@otago.ac.nz>
2019-05-28 17:47:43 +1200

    Tied dependencies for slurm commands

--- Annotate_my_VCF.sh
+++ Annotate_my_VCF.sh
@@ -1,5 +1,176 @@
 #!/bin/bash
 
+#################################################################################################
+# some useful functions from Sam's baserefs.sh
+
+#####################
+# Return a string with a new item on the end
+# Items are separated by a char or space
+#####################
+function appendList {
+        oldList="${1}"
+        newItem="${2}"
+        itemJoiner=$([ "${3}" == "" ] && echo -ne " " || echo -ne "${3}")       # If blank, use space.
+        if [ "$newItem" != "" ]; then
+                if [ "$oldList" == "" ]; then
+                        # Initial Entry
+                        printf "%s" "$newItem"
+                else
+                        # Additional Entry
+                        printf "%s%s%s" "$oldList" "$itemJoiner" "$newItem"
+                fi
+        else
+                printf "%s" "$oldList"
+        fi
+}
+export -f appendList
+
+###################
+# Outputs a dependency if one exists.
+###################
+function depCheck {
+	#(echo "depCheck: $0 \"${@}\"" | tee -a ~/depCheck.txt 1>&2)
+	local jobList=$(jobsExist "$1")
+	[ "$jobList" != "" ] && echo -ne "--dependency afterok:${jobList}"
+}
+export -f depCheck
+
+####################
+# Returns true if the list of jobs passed are running or waiting to run, otherwise false
+####################
+function jobsExist {
+	local jobList=""
+	local IFS=':'
+	for item in $@
+	do
+		if squeue -j $item | grep "$item" &>/dev/null
+		then
+			jobList=$(appendList "$jobList" $item ":")
+		fi
+	done
+	echo "$jobList"
+}
+export -f jobsExist
+
+###################
+# Visual ticker
+###################
+function tickOver {
+	case "$TICKER" in
+		"|") TICKER="/" ;;
+		"/") TICKER="-" ;;
+		"-") TICKER="\\" ;;
+		*) TICKER="|" ;;
+	esac
+	
+	>&2 printf "%s\b" "$TICKER"
+}
+export -f tickOver
+
+#######################
+# Condenses list of numbers to ranges
+#
+# 1,2,3,4,7,8,9,12,13,14 -> 1-3,4,7-9,12-14
+#######################
+function condenseList {
+	echo "${@}," | \
+		sed "s/,/\n/g" | \
+		while read num
+		do
+		if [[ -z $first ]]
+		then
+			first=$num
+			last=$num
+			continue
+		fi
+		if [[ num -ne $((last + 1)) ]]
+		then
+			if [[ first -eq last ]]
+			then
+				echo $first
+			else
+				echo $first-$last
+			fi
+			first=$num
+			last=$num
+		else
+			: $((last++))
+		fi
+	done | paste -sd ","
+}
+export -f condenseList
+
+#####################
+# Expand comma separated list of ranges to individual elements
+#
+# 1,3-5,8,10-12 -> 1,3,4,5,8,10,11,12
+#####################
+function expandList {
+	for f in ${1//,/ }; do
+		if [[ $f =~ - ]]; then
+			a+=( $(seq ${f%-*} 1 ${f#*-}) )
+		else
+			a+=( $f )
+		fi  
+	done
+	
+	a=${a[*]}
+	a=${a// /,}
+	
+	echo $a
+}
+export -f expandList
+
+######################
+# Return the string with the split char replaced by a space
+######################
+function splitByChar {
+	input=${1}
+	char=${2}
+	
+	if [ "$char" != "" ]; then
+		echo -ne "$input" | sed -e "s/${char}/ /g"
+	else
+		echo -ne "FAIL"
+		(>&2 echo -ne "FAILURE:\t splitByChar [${1}] [${2}]\n\tNo character to replace. You forget to quote your input?\n")
+	fi
+}
+export -f splitByChar
+
+######################
+# Find matching task elements in a parent and child array.
+# Set the child array task element to be dependent on the matching parent task element.
+######################
+function tieTaskDeps {
+	childArray=$(expandList ${1})
+	childJobID=${2}
+	parentArray=$(expandList ${3})
+	parentJobID=${4}
+	
+	if [ "$childArray" != "" ] && [ "$parentArray" != "" ]; then
+		# Both arrays contain something.
+		# Cycle through child array elements
+		for i in $(splitByChar "$childArray" ","); do
+			elementMatched=0
+			# Cycle through parent array elements.
+			for j in $(splitByChar "$parentArray" ","); do
+				if [ "$i" == "$j" ]; then
+					# Matching element found. Tie child element to parent element.
+#					printf " T[%s->%s] " "${childJobID}_$i" "${parentJobID}_$j"
+					scontrol update JobId=${childJobID}_$i Dependency=afterok:${parentJobID}_$j
+					elementMatched=1
+				fi
+			done
+			if [ $elementMatched -eq 0 ]; then
+				# No matching element found in parent array.
+				# Release child element from entire parent array.
+				scontrol update JobId=${childJobID}_$i Dependency=
+			fi
+			tickOver
+		done
+	fi
+}
+export -f tieTaskDeps
 
 ##########################################################################
 function mybanner {
@@ -964,155 +1135,130 @@ if ! mkdir -p ${PROJECT_PATH}/slurm; then
 fi
 launch=$PWD
 cd ${PROJECT_PATH}
-
 if [[ ${snpeff} == "yes" ]]; then
-	for CONTIG in ${CONTIGARRAY[@]}; do
-		if [ ! -f "${PROJECT_PATH}/done/snpeff/${CONTIG}_SnpEff.vcf.gz.tbi.done" ] && [ ! -f "${PROJECT_PATH}/done/ann/${CONTIG}_ann.vcf.gz.tbi.done" ]; then
-			snpeffrun=incomplete
-#			echo -e "${CONTIG} SnpEff annotation incomplete"
-		fi
+	mkdir -p ${PROJECT_PATH}/slurm/snpeff
+	snpeffArray=""
+	for i in $(seq 1 ${#CONTIGARRAY[@]}); do
+		CONTIG=${CONTIGARRAY[$(( ${i} - 1 ))]}
+    	if [ ! -f ${PROJECT_PATH}/done/snpeff/${CONTIG}_SnpEff.vcf.gz.tbi.done ]; then
+        	snpeffArray=$(appendList "${snpeffArray}"  $i ",")
+    	fi
 	done
-
-	if [[ ${snpeffrun} == incomplete ]]; then
-		if ! mkdir -p ${PROJECT_PATH}/slurm/snpeff; then
-			echo "Error creating output folder!"
+	if [ "$snpeffArray" != "" ]; then
+		snpeffjob=$(sbatch -J SnpEff_${PROJECT} ${mailme} --array ${snpeffArray}%12 ${BASEDIR}/slurm_scripts/annoSnpEff.sl | awk '{print $4}')
+		if [ $? -ne 0 ] || [ "$snpeffjob" == "" ]; then
+			(printf "FAILED!\n" 1>&2)
 			exit 1
+		else
+			echo "SnpEff_${PROJECT} job is ${snpeffjob}"
+			(printf "%sx%-4d [%s] Logs @ %s\n" "$snpeffjob" $(splitByChar "$snpeffArray" "," | wc -w) $(condenseList "$snpeffArray") "${PROJECT_PATH}/slurm/snpeff/snpeff-${snpeffjob}_*.out" 1>&2)
 		fi
-cmd="sbatch -J SnpEff_${PROJECT} ${mailme} --array 1-${contigarraynumber}%12 ${BASEDIR}/slurm_scripts/annoSnpEff.sl"
-		snpeffjob=$(eval $cmd | awk '{print $4}')
-		echo "SnpEff_${PROJECT} job is ${snpeffjob}"
 	fi
 fi
+
 
 if [[ ${vep} == "yes" ]]; then
-	for CONTIG in ${CONTIGARRAY[@]}; do
-		if [ ! -f "${PROJECT_PATH}/done/vep/${CONTIG}_VEP.vcf.gz.tbi.done" ] && [ ! -f "${PROJECT_PATH}/done/ann/${CONTIG}_ann.vcf.gz.tbi.done" ]; then
-			veprun=incomplete
-		fi
+	mkdir -p ${PROJECT_PATH}/slurm/vep
+	vepArray=""
+	for i in $(seq 1 ${#CONTIGARRAY[@]}); do
+		CONTIG=${CONTIGARRAY[$(( ${i} - 1 ))]}
+		if [ ! -f ${PROJECT_PATH}/done/vep/${CONTIG}_VEP.vcf.gz.tbi.done ]; then
+        	vepArray=$(appendList "${vepArray}"  $i ",")
+    	fi
 	done
-	
-	if [ ! -z ${snpeffjob} ]; then
-		depend="--dependency=aftercorr:${snpeffjob}"
-	else 
-		depend=""
-	fi
-
-	if [[ ${veprun} == incomplete ]]; then
-		if ! mkdir -p ${PROJECT_PATH}/slurm/vep; then
-			echo "Error creating output folder!"
+	if [ "$vepArray" != "" ]; then
+		vepjob=$(sbatch -J VEP_${PROJECT} $(depCheck $snpeffjob) ${mailme} --array ${vepArray}%12 ${BASEDIR}/slurm_scripts/annoVEP.sl | awk '{print $4}')
+		if [ $? -ne 0 ] || [ "$vepjob" == "" ]; then
+			(printf "FAILED!\n" 1>&2)
 			exit 1
+		else
+			echo "VEP_${PROJECT} job is ${vepjob}"
+			# Tie each task to the matching task in the previous array.
+			if [ ${snpeff} == "yes" ]; then
+				tieTaskDeps "$vepArray" "$vepjob" "$snpeffArray" "$snpeffjob"
+			fi
+			(printf "%sx%-4d [%s] Logs @ %s\n" "$vepjob" $(splitByChar "$vepArray" "," | wc -w) $(condenseList "$vepArray") "${PROJECT_PATH}/slurm/vep/vep-${vepjob}_*.out" 1>&2)
 		fi
-		cmd="sbatch -J VEP_${PROJECT} ${depend} ${mailme} --array 1-${contigarraynumber}%12 ${BASEDIR}/slurm_scripts/annoVEP.sl"
-		vepjob=$(eval $cmd | awk '{print $4}')
-		echo "VEP_${PROJECT} job is ${vepjob}"
 	fi
 fi
 
-for CONTIG in ${CONTIGARRAY[@]}; do
-	if [ ! -f "${PROJECT_PATH}/done/ann/${CONTIG}_ann.vcf.gz.tbi.done" ]; then
-		anno=incomplete
-#		echo -e "${CONTIG} annotation incomplete"
+if [ "${sourcetagarray[@]}" == "" ]; then
+	mkdir -p ${PROJECT_PATH}/slurm/ann
+	annArray=""
+	for i in $(seq 1 ${#CONTIGARRAY[@]}); do
+		CONTIG=${CONTIGARRAY[$(( ${i} - 1 ))]}
+		if [ ! -f ${PROJECT_PATH}/done/ann/${CONTIG}_ann.vcf.gz.tbi.done ]; then
+  	      annArray=$(appendList "${annArray}"  $i ",")
+  	  fi
+	done
+	if [ "$annArray" != "" ]; then
+		annjob=$(sbatch -J AnnotateVCF_${PROJECT} $(depCheck $snpeffjob $vepjob) ${mailme} --array 1-${contigarraynumber}%12 ${BASEDIR}/slurm_scripts/annotateVCF.sl | awk '{print $4}')
+		if [ $? -ne 0 ] || [ "$annjob" == "" ]; then
+			(printf "FAILED!\n" 1>&2)
+			exit 1
+		else
+			echo "Ann_${PROJECT} job is ${annjob}"
+			if [ ${vep} == "yes" ]; then
+				tieTaskDeps "$annArray" "$annjob" "$vepArray" "$vepjob"
+			elif [ ${snpeff} == "yes" ]; then
+				tieTaskDeps "$annArray" "$annjob" "$snpeffArray" "$snpeffjob"
+			fi
+			(printf "%sx%-4d [%s] Logs @ %s\n" "$annjob" $(splitByChar "$annArray" "," | wc -w) $(condenseList "$annArray") "${PROJECT_PATH}/slurm/ann/ann-${annjob}_*.out" 1>&2)
+		fi
 	fi
-done
-
-if [ ! -z ${vepjob} ]; then
-	depend="--dependency=aftercorr:${vepjob}"
-else 
-	depend=""
 fi
-
-if [[ ${anno} == incomplete ]]; then
-	if ! mkdir -p ${PROJECT_PATH}/slurm/ann; then
-		echo "Error creating output folder!"
-		exit 1
-	fi
-	cmd="sbatch -J AnnotateVCF_${PROJECT} ${depend} ${mailme} --array 1-${contigarraynumber}%12 ${BASEDIR}/slurm_scripts/annotateVCF.sl"
-	annodep=$(eval $cmd | awk '{print $4}')
-	echo "Annotate_${PROJECT} job is ${annodep}"
-fi
-
 
 #### Merge the annotated contig vcf.gz files together
-if [ ! -z ${annodep} ]; then
-	depend="--dependency=afterok:${annodep}"
-else 
-	depend=""
-fi
-
+mkdir -p ${PROJECT_PATH}/slurm/merge
 if [ ! -f "${PROJECT_PATH}/done/merge/${PROJECT}.done" ]; then
-	merge="incomplete"
-fi
-
-if [[ ${merge} == "incomplete" ]]; then
-	if ! mkdir -p ${PROJECT_PATH}/slurm/merge; then
-		echo "Error creating output folder!"
+	mergejob=$(sbatch $(depCheck $snpeffjob $vepjob $annjob) -J Merge_${PROJECT} ${mailme} ${BASEDIR}/slurm_scripts/merge.sl | awk '{print $4}')
+	if [ $? -ne 0 ] || [ "$mergejob" == "" ]; then
+		(printf "FAILED!\n" 1>&2)
 		exit 1
+	else
+		echo "Merge_${PROJECT} job is ${mergejob}"
+		(printf "%s Log @ %s\n" "$mergejob" "${PROJECT_PATH}/slurm/merge/merge-${mergejob}.out" 1>&2)
 	fi
-
-	cmd="sbatch $depend -J Merge_${PROJECT} ${mailme} ${BASEDIR}/slurm_scripts/merge.sl"
-	mergedep=$(eval $cmd | awk '{print $4}')
-	echo "Merge_${PROJECT} job is ${mergedep}"
 fi
 
 
 ###### Do the BCSQ annotatiom
 if [[ ${BCSQ} == "yes" ]]; then
 	if [ ! -f "${PROJECT_PATH}/done/bcsq/${PROJECT}_ann.vcf.gz.tbi.done" ]; then
-			bcsqrun=incomplete
-	fi
-	if [ ! -z ${mergedep} ]; then
-		depend="--dependency=afterok:${mergedep}"
-	else 
-		depend=""
-	fi
-
-	if [[ ${bcsqrun} == incomplete ]]; then
-		if ! mkdir -p ${PROJECT_PATH}/slurm/bcsq; then
-			echo "Error creating output folder!"
+		bcsqjob=$(sbatch -J BCSQ_${PROJECT} $(depCheck $mergejob) ${mailme} ${BASEDIR}/slurm_scripts/annoBCSQ.sl | awk '{print $4}')
+		if [ $? -ne 0 ] || [ "$bcsqjob" == "" ]; then
+			(printf "FAILED!\n" 1>&2)
 			exit 1
+		else
+			echo "BCSQ_${PROJECT} job is ${bcsqjob}"
+			(printf "%s Log @ %s\n" "$bcsqjob" "${PROJECT_PATH}/slurm/bcsq/bcsq-${bcsqjob}.out" 1>&2)
 		fi
-		cmd="sbatch -J BCSQ_${PROJECT} ${depend} ${mailme} ${BASEDIR}/slurm_scripts/annoBCSQ.sl"
-		bcsqjob=$(eval $cmd | awk '{print $4}')
-		echo "BCSQ_${PROJECT} job is ${bcsqjob}"
 	fi
 fi
 
 ##### Move most files to destination directory
+mkdir -p ${PROJECT_PATH}/slurm/move
 if [ ! -f "${PROJECT_PATH}/done/move/${PROJECT}.done" ]; then
-	move=incomplete
-fi
-if [[ ${move} == incomplete ]]; then
-	if [[ ${BCSQ} == "yes" ]]; then
-		if [ ! -z ${bcsqjob} ]; then
-			depend="--dependency=afterok:${bcsqjob}"
-		else 
-			depend=""
-		fi
-	else
-		if [ ! -z ${mergedep} ]; then
-			depend="--dependency=afterok:${mergedep}"
-		else 
-			depend=""
-		fi
-	fi
-	if ! mkdir -p ${PROJECT_PATH}/slurm/move; then
-		echo "Error creating output folder!"
+	movejob=$(sbatch -J Move_${PROJECT} $(depCheck $mergejob $bcsqjob) ${mailme} ${BASEDIR}/slurm_scripts/move.sl | awk '{print $4}')
+	if [ $? -ne 0 ] || [ "$movejob" == "" ]; then
+		(printf "FAILED!\n" 1>&2)
 		exit 1
+	else
+		echo "Move_${PROJECT} job is ${movejob}"
+		(printf "%s Log @ %s\n" "$movejob" "${PROJECT_PATH}/slurm/move/move-${movejob}.out" 1>&2)
 	fi
-	cmd="sbatch -J Move_${PROJECT} ${depend} ${mailme} ${BASEDIR}/slurm_scripts/move.sl"
-	movejob=$(eval $cmd | awk '{print $4}')
-	echo "Move_${PROJECT} job is ${movejob}"
 fi
 
 #####clean up 
 #this takes us back to the original launch directory so that the final slurm out file will be written there
 cd ${launch}
-if [ ! -z ${movejob} ] || [[ ${move} == incomplete ]]; then
-	depend="--dependency=afterok:${movejob}"
-else 
-	depend=""
-fi
-cmd="sbatch -J Cleanup_${PROJECT} ${depend} ${mailme} ${BASEDIR}/slurm_scripts/cleanup.sl"
+cleanupjob=$(sbatch -J Cleanup_${PROJECT} ${depend} ${mailme} ${BASEDIR}/slurm_scripts/cleanup.sl | awk '{print $4}')
 cleanupjob=$(eval $cmd | awk '{print $4}')
-echo "Cleanup_${PROJECT} job is ${cleanupjob}"
+if [ $? -ne 0 ] || [ "$cleanupjob" == "" ]; then
+	(printf "FAILED!\n" 1>&2)
+	exit 1
+else
+	echo "Cleanup_${PROJECT} job is ${cleanupjob}"
+	(printf "%s Log @ %s\n" "$cleanupjob" "${launch}/AnnotationCleanup-${cleanupjob}.out" 1>&2)
+fi
 
